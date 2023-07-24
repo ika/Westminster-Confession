d6943a92b964f9bb112b542d24befdda54ccf9a0
Ian Armstrong <ian.k.armstrong@gmail.com>
2023-07-24 12:09:55 +0200

    update

--- lib/utils/tx_size.dart
+++ lib/utils/tx_size.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:westminster_confession/cubit/cub_text.dart';
-import 'package:westminster_confession/utils/globals.dart';
 import 'package:westminster_confession/utils/shared_prefs.dart';
-import 'package:westminster_confession/west/we_main.dart';
 
 //MaterialColor? primarySwatch;
 SharedPrefs sharedPrefs = SharedPrefs();
